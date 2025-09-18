8f13aff4d40a1cee1b8985052a0681cb1c318f8e
qiaoxxyxx <qiao58106@gmail.com>
2025-09-18 23:17:47 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,7 +2,7 @@
 -- 作者: XXYXX
 -- 版本: v2
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/qiaoxxyxx/Roblox/blob/main/main.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/koboyads/qiaoxxyxx/Roblox/main/main.lua"))()
 
 -- 防AFK机制
 local VirtualUser = game:GetService("VirtualUser")
@@ -288,5 +288,6 @@ end)
 Library:Notify("Welcome", "脚本加载成功！")
 
 print("XXYXX GOD 已加载")
+
 
 
